05547037290787168038b0a38c3bea7c5697e975
jezykowski7 <jamjestjezyk@gmail.com>
2025-11-27 13:51:35 +0100

    Update wiecej.js

--- Kalkulator-aplikacja/wiecej/wiecej.js
+++ Kalkulator-aplikacja/wiecej/wiecej.js
@@ -5,21 +5,20 @@ let historia = [];
 historia.push(ostatnieDzialanie);
 console.table(historia);
 
-const htmlEl = document.documentElement;
-
 // Ustawienie motywu przy starcie
+const html = document.getElementById("html");
 if(localStorage.getItem("motyw") === "1") {
-    htmlEl.classList.add("ciemny");
+    html.classList.add("ciemny");
     document.getElementById("ikona").classList.add("icon-moon-inv");
 } else {
-    htmlEl.classList.remove("ciemny");
+    html.classList.remove("ciemny");
 }
 
 // Obsługa przycisku zmiany motywu
 document.getElementById("motyw").addEventListener("click", () => {
-    const isDark = htmlEl.classList.toggle("ciemny"); // true jeśli teraz jest ciemny
+    const czyciemny = html.classList.toggle("ciemny"); // true jeśli teraz jest ciemny
     document.getElementById("ikona").classList.toggle("icon-moon-inv");
 
     // Zapis do localStorage (1 = ciemny, 0 = jasny)
-    localStorage.setItem("motyw", isDark ? "1" : "0");
+    localStorage.setItem("motyw", czyciemny ? "1" : "0");
 });
